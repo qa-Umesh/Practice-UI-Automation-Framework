7e2ad55fb27291652289234f65708f5ffa4696fd
Umesh Kute <umeshkute453@gmail.com>
2026-04-29 12:56:46 +0530

    Merge branch 'master' of https://github.com/qa-Umesh/Practice-UI-Automation-Framework.git

--- src/test/java/com/utility/BrowserUtility.java
+++ src/test/java/com/utility/BrowserUtility.java
@@ -108,7 +108,7 @@ public abstract class BrowserUtility {
 	public String getScreenShot(String name) {
 		TakesScreenshot ts = (TakesScreenshot) driver.get();
 		File scource = ts.getScreenshotAs(OutputType.FILE);
-		String fileaPath="./SreenShots/"+name+".png";
+		String fileaPath=".SreenShots/"+name+".png";
 		File path = new File(fileaPath);
 		try {
 			FileUtils.copyFile(scource, path);
